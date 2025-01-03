c4e0236ff79742569c25669dd7c5e5f140668b87
Rohan Shrestha <shrestharohan039@gmail.com>
2025-01-03 13:35:59 +0545

    feat: all necessary setups

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'api/v',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Hamro Events API')
+    .setDescription('API for Event Management System')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
